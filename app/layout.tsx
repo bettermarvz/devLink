0bdd067e48d6faaa5fae282222fe080327d4b34c
MarvZ <marvs@betterteem.com>
2025-09-16 20:15:45 +0800

    fix: manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.ts" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="theme-color" content="#000000" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
       </head>
